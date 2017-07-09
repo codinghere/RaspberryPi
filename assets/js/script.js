8d71b26eeca1fbc141698df9b8e648aee27c0bef
eyllanesc <e.yllanescucho@gmail.com>
2017-07-09 16:27:32 -0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,5 @@
 $(function () {
 
-    function GetValues() {
-        $list = [];
-        $('#user-datalist').children().each(function () {
-            var value = $(this).val();
-            var id = $(this).attr('id');
-            var item = {id: id,value: value};
-            $list.push(item);
-        });
-        return $list;
-    }
-
-    console.log(GetValues());
-    
     var container = $('#pagination-demo');
 
     var sources  = ['1', '2', '3'];
