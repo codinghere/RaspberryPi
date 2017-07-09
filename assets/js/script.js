b7cb637e94002903d6221d33fca28713ceab01f3
eyllanesc <e.yllanescucho@gmail.com>
2017-07-09 13:51:57 -0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,37 @@
- var container = $('#pagination-pagination-demo');
+ 
+$(function () {
 
-var sources = function () {
-    var result = [];
-    for (var i = 1; i < 196; i++) {
-        result.push(i);
-    }
-    return result;
+    var container = $('#pagination-pagination-demo');
+
+    var sources = function () {
+        var result = [];
+        for (var i = 1; i < 196; i++) {
+            result.push(i);
+        }
+        return result;
+    }();
+
+    var options = {
+        dataSource: sources,
+        callback: function (response, pagination) {
+            window.console && console.log(response, pagination);
+            var dataHtml = '<ul>';
+            $.each(response, function (index, item) {
+             dataHtml += '<li>' + item + '</li>';
+         });
+
+            dataHtml += '</ul>';
+            container.prev().html(dataHtml);
+        }
+    };
+                //$.pagination(container, options);
+    container.addHook('beforeInit', function () {
+        window.console && console.log('beforeInit...');
+    });
+
+    container.pagination(options);
+    container.addHook('beforePageOnClick', function () {
+    window.console && console.log('beforePageOnClick...');
+     //return false
+    });
 }();
-
-var options = {
-    dataSource: sources,
-    callback: function (response, pagination) {
-        window.console && console.log(response, pagination);
-        var dataHtml = '<ul>';
-        $.each(response, function (index, item) {
-         dataHtml += '<li>' + item + '</li>';
-     });
-
-        dataHtml += '</ul>';
-        container.prev().html(dataHtml);
-    }
-};
-            //$.pagination(container, options);
-container.addHook('beforeInit', function () {
-    window.console && console.log('beforeInit...');
-});
-
-container.pagination(options);
-container.addHook('beforePageOnClick', function () {
-window.console && console.log('beforePageOnClick...');
- //return false
-});
