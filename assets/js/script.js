e53449da67318427e6c501c769c38c5457e71255
eyllanesc <e.yllanescucho@gmail.com>
2017-07-09 14:17:15 -0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,33 @@
-    $(function () {
-        function createDemo(name) {
-            var container = $('#pagination-' + name);
-            var sources = function () {
-                var result = [];
-                for (var i = 1; i < 196; i++) {
-                    result.push(i);
-                }
-                return result;
-            }();
-            var options = {
-                dataSource: sources,
-                callback: function (response, pagination) {
-                    window.console && console.log(response, pagination);
-                    var dataHtml = '<ul>';
-                    $.each(response, function (index, item) {
-                        dataHtml += '<li>' + item + '</li>';
-                    });
-                    dataHtml += '</ul>';
-                    console.log(dataHtml);
-                    $('#data-container').html(dataHtml);
-                }
-            };
-            //$.pagination(container, options);
-            container.addHook('beforeInit', function () {
-                window.console && console.log('beforeInit...');
+$(function () {
+    var container = $('#pagination-demo');
+    var sources = function () {
+        var result = [];
+        for (var i = 1; i < 196; i++) {
+            result.push(i);
+        }
+        return result;
+    }();
+    
+    var options = {
+        dataSource: sources,
+        callback: function (response, pagination) {
+            window.console && console.log(response, pagination);
+            var dataHtml = '<ul>';
+            $.each(response, function (index, item) {
+                dataHtml += '<li>' + item + '</li>';
             });
-            container.pagination(options);
-            container.addHook('beforePageOnClick', function () {
-                window.console && console.log('beforePageOnClick...');
+            dataHtml += '</ul>';
+            $('#data-container').html(dataHtml);
+        }
+    };
+            //$.pagination(container, options);
+    container.addHook('beforeInit', function () {
+        window.console && console.log('beforeInit...');
+    });
+    container.pagination(options);
+    container.addHook('beforePageOnClick', function () {
+        window.console && console.log('beforePageOnClick...');
                 //return false
             });
-            return container;
-        }
-        createDemo('demo');
-    });
+    return container;
+});
