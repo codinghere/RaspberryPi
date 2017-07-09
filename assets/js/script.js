877b9fc755d6ed0515dd92fbd42909c21a00d563
eyllanesc <e.yllanescucho@gmail.com>
2017-07-09 13:55:20 -0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,35 @@
- 
-$(function () {
-
-    var container = $('#pagination-pagination-demo');
-
-    var sources = function () {
-        var result = [];
-        for (var i = 1; i < 196; i++) {
-            result.push(i);
+    $(function () {
+        function createDemo(name) {
+            var container = $('#pagination-' + name);
+            var sources = function () {
+                var result = [];
+                for (var i = 1; i < 196; i++) {
+                    result.push(i);
+                }
+                return result;
+            }();
+            var options = {
+                dataSource: sources,
+                callback: function (response, pagination) {
+                    window.console && console.log(response, pagination);
+                    var dataHtml = '<ul>';
+                    $.each(response, function (index, item) {
+                        dataHtml += '<li>' + item + '</li>';
+                    });
+                    dataHtml += '</ul>';
+                    container.prev().html(dataHtml);
+                }
+            };
+            //$.pagination(container, options);
+            container.addHook('beforeInit', function () {
+                window.console && console.log('beforeInit...');
+            });
+            container.pagination(options);
+            container.addHook('beforePageOnClick', function () {
+                window.console && console.log('beforePageOnClick...');
+                //return false
+            });
+            return container;
         }
-        return result;
-    }();
-
-    var options = {
-        dataSource: sources,
-        callback: function (response, pagination) {
-            window.console && console.log(response, pagination);
-            var dataHtml = '<ul>';
-            $.each(response, function (index, item) {
-             dataHtml += '<li>' + item + '</li>';
-         });
-
-            dataHtml += '</ul>';
-            container.prev().html(dataHtml);
-        }
-    };
-                //$.pagination(container, options);
-    container.addHook('beforeInit', function () {
-        window.console && console.log('beforeInit...');
+        createDemo('demo');
     });
-
-    container.pagination(options);
-    container.addHook('beforePageOnClick', function () {
-    window.console && console.log('beforePageOnClick...');
-     //return false
-    });
-});
