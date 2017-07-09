dba95482ba8a6d19fc91b62267b762c6342c3baf
eyllanesc <e.yllanescucho@gmail.com>
2017-07-09 11:59:36 -0500

    .appendTo('body')

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    document.getElementById("pager").pagination({
+    document.getElementById("pager").appendTo('body').pagination({
         items: 100,
         itemsOnPage: 10,
         cssStyle: 'light-theme'
